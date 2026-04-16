aaa755668d0c6f0c6ae7508e61b52178891fb306
VitaoIF <victormelo1212@gmail.com>
2026-04-16 20:27:03 -0300

    Feat: Adiciona o BookController

---
+++ LibFlowAPI/src/main/java/com/projetos/LibFlowAPI/controllers/BookController.java
@@ -0,0 +1,50 @@
+package com.projetos.LibFlowAPI.controllers;
+
+import com.projetos.LibFlowAPI.entities.Book;
+import com.projetos.LibFlowAPI.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @PostMapping
+    public ResponseEntity<Book> insert(@RequestBody Book book){
+        book = bookService.insert(book);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(book.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(book);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Book>> findAll(){
+        List<Book> bookList = bookService.findAll();
+        return ResponseEntity.ok().body(bookList);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Book> findById(@PathVariable Long id){
+        Book bookId = bookService.findById(id);
+        return ResponseEntity.ok().body(bookId);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        bookService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
