4ae6215b1037b478835e70949d36e7cf84203f3a
VitaoIF <victormelo1212@gmail.com>
2026-04-17 13:16:31 -0300

    refactor: adiciona annotations e getters e setters

--- LibFlowAPI/src/main/java/com/projetos/LibFlowAPI/entities/Loan.java
+++ LibFlowAPI/src/main/java/com/projetos/LibFlowAPI/entities/Loan.java
@@ -1,14 +1,98 @@
 package com.projetos.LibFlowAPI.entities;
 
 import com.projetos.LibFlowAPI.entities.enums.LoanStatus;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
+@Entity
+@Table(name = "tb_loan")
 public class Loan {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LoanStatus status;
-
     private LocalDate loanDate;
     private LocalDate returnDate;
+
+    @ManyToOne
+    private Reader reader;
+
+    @ManyToOne
+    private Book book;
+
+    public Loan() {
+    }
+
+    public Loan(Book book, Long id, LocalDate loanDate, Reader reader, LocalDate returnDate, LoanStatus status) {
+        this.book = book;
+        this.id = id;
+        this.loanDate = loanDate;
+        this.reader = reader;
+        this.returnDate = returnDate;
+        this.status = status;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getLoanDate() {
+        return loanDate;
+    }
+
+    public void setLoanDate(LocalDate loanDate) {
+        this.loanDate = loanDate;
+    }
+
+    public Reader getReader() {
+        return reader;
+    }
+
+    public void setReader(Reader reader) {
+        this.reader = reader;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public LoanStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(LoanStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Loan loan = (Loan) o;
+        return Objects.equals(id, loan.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
